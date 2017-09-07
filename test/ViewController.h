96ef6d37426f02b033c231acd059b8a16773b2d0
Gaurav <gaurav@phoniex.local>
2017-09-07 17:41:30 +0530

    youtube integration

--- test/ViewController.h
+++ test/ViewController.h
@@ -7,9 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "YTPlayerView.h"
 
 @interface ViewController : UIViewController
-
 
 @end
 
--- test/ViewController.m
+++ test/ViewController.m
@@ -13,7 +13,10 @@
 #import <AVFoundation/AVFoundation.h>
 #import <AVKit/AVKit.h>
 
-@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource> {
+    YTPlayerView *objPlayerView;
+}
 
 @property(nonatomic,weak) IBOutlet UITableView *tblView;
 
@@ -63,14 +66,26 @@
     NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
     AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
 
+    UIActivityIndicatorView *activityView = [[UIActivityIndicatorView alloc]
+                                             initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    
+    activityView.center=self.view.center;
+    [activityView startAnimating];
+    [self.view addSubview:activityView];
+    
+    [[UIApplication sharedApplication] beginIgnoringInteractionEvents];
     
     NSString *URLString = @"http://date.jsontest.com/";
-    NSDictionary *parameters = nil;
+    NSDictionary *parameters = [NSDictionary dictionaryWithObject:@"cb784502bffab6e" forKey:@"key"];
     
     NSMutableURLRequest *request = [[AFHTTPRequestSerializer serializer] requestWithMethod:@"POST" URLString:URLString parameters:parameters error:nil];
   //  [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
     
     NSURLSessionDataTask *dataTask = [manager dataTaskWithRequest:request completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
+        
+        [activityView stopAnimating];
+        [[UIApplication sharedApplication] endIgnoringInteractionEvents];
+        
         if (error) {
             NSLog(@"Error: %@", error);
         } else {
@@ -149,7 +164,20 @@
 // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
     MyTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MyTableViewCell"];
+    
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    
+    cell.lblLabel.text=[NSString stringWithFormat:@"test %ld",(long)indexPath.row];
+    cell.lblDesc.text=[NSString stringWithFormat:@"desc %ld",(long)indexPath.row];
+    //
+    //cancel loading previous image for cell
+    [[AsyncImageLoader sharedLoader] cancelLoadingImagesForTarget:cell.imgView];
+    
+    [cell.imgView setImageURL:[NSURL URLWithString:@"http://charcoaldesign.co.uk/AsyncImageView/Forest/IMG_0351.JPG"]];
+    
+  /*  MyTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MyTableViewCell"];
     
     if (cell==nil) {
         NSArray *nibs = [[NSBundle mainBundle] loadNibNamed:@"MyTableViewCell" owner:self options:nil];
@@ -163,7 +191,7 @@
     //cancel loading previous image for cell
     [[AsyncImageLoader sharedLoader] cancelLoadingImagesForTarget:cell.imgView];
 
-    [cell.imgView setImageURL:[NSURL URLWithString:@"http://charcoaldesign.co.uk/AsyncImageView/Forest/IMG_0351.JPG"]];
+    [cell.imgView setImageURL:[NSURL URLWithString:@"http://charcoaldesign.co.uk/AsyncImageView/Forest/IMG_0351.JPG"]];*/
     
  /*   static NSString *reuseIdentifier = @"cellIdentifier";
     
@@ -180,19 +208,31 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
-    AVPlayerViewController * _moviePlayer1 = [[AVPlayerViewController alloc] init];
-    _moviePlayer1.player = [AVPlayer playerWithURL:[NSURL URLWithString:@"https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"]];
+//    AVPlayerViewController * _moviePlayer1 = [[AVPlayerViewController alloc] init];
+//    _moviePlayer1.player = [AVPlayer playerWithURL:[NSURL URLWithString:@"https://www.youtube.com/watch?v=pX53bOO-6ak"]];
+//    
+//    //https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4
+//    [self presentViewController:_moviePlayer1 animated:YES completion:^{
+//        [_moviePlayer1.player play];
+//    }];
+    NSString *url = @"https://www.youtube.com/watch?v=pX53bOO-6ak";
     
-    [self presentViewController:_moviePlayer1 animated:YES completion:^{
-        [_moviePlayer1.player play];
-    }];
+    NSArray *strUrl = [url componentsSeparatedByString:@"?v="];
+    NSString *videoid = [strUrl objectAtIndex:1];
     
+    if (!objPlayerView) {
+        objPlayerView = [[YTPlayerView alloc] initWithFrame:CGRectMake(0, 0, 200,  200)];
+    }
+    [objPlayerView setHidden:NO];
+    [self.view addSubview:objPlayerView];
+
+     [objPlayerView loadWithVideoId:videoid];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 @end
